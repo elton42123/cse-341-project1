cab6aaabe18b96f0494a73b6de5d2fe0f8b1e134
elton42123 <ielton42123@gmail.com>
2025-11-04 17:44:12 +0300

    week1

---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controller/users');
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
+module.exports = router;
