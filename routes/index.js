a4d3c7e4eb3a0818c911f6acd34fc9f8533ff4db
elton42123 <ielton42123@gmail.com>
2025-11-08 12:33:04 +0300

    .

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//router.get('/', (req, res) => {res.send('Welcome to the Contacts API');});
+router.get('/', (req, res) => {res.send('Welcome to the Contacts API');});
 router.use('/users', require('./users'));
 
 module.exports = router;
